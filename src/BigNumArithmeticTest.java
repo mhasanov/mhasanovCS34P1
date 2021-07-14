9ef778497ab2d211170d007aee752040ca3ea3e8
mhasanov <49108810+mhasanov@users.noreply.github.com>
2021-07-14 16:19:35 -0400

    Update BigNumArithmeticTest.java

--- src/BigNumArithmeticTest.java
+++ src/BigNumArithmeticTest.java
@@ -1,13 +1,19 @@
 import student.TestCase;
 
 /**
- * 
- */
-
-/**
- * @author mehdi
+ * @author mhasanov
  *
  */
 public class BigNumArithmeticTest extends TestCase {
 
+  LinkedList<Integer> LL;
+  
+  public void setUp(){
+    LL = new LinkedList<Integer>();
+  }
+  
+  public void testLinkedList(){
+    
+  }
+  
 }
