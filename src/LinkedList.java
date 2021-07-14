671871be4b6059098dbcbbf13839a71f9015a3f7
leohan95 <53951375+leohan95@users.noreply.github.com>
2021-07-15 05:22:28 +0900

    Update LinkedList.java

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -40,6 +40,7 @@ public class LinkedList<T> implements ListInterface<T> {
         String ans = "";
         for (int i=size-1; i > 0; i--) {
              ans += get(i).data.toString();
+        }
     }
     
     
