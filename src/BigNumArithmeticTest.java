ceeabd75e8ab375a85f853c102da5fd9d2c8c024
mhasanov <49108810+mhasanov@users.noreply.github.com>
2021-07-14 20:06:39 -0400

    Update BigNumArithmeticTest.java

--- src/BigNumArithmeticTest.java
+++ src/BigNumArithmeticTest.java
@@ -71,45 +71,48 @@ public class BigNumArithmeticTest extends TestCase {
      * Compares return value and expected value
      */
   public void testPeek() {
-        LL.push(3);
-        assertEquals((int)LL.peek(), 3);
-        LL = new LinkedList<Integer>();
+      LL.push(3);
+      assertEquals((int)LL.peek(), 3);
+      LL = new LinkedList<Integer>();
   }
   
   /**
    * Tests Addition method
    */
   public void testAddition() {
-        LL.push(1);
-        LL.push(2);
-        LL.push(7);
-        LL2.push(1);
-        LL2.push(2);
-        LL2.push(3);
-        LinkedList<Integer> LL3 = new LinkedList<Integer>();
-        LL3.push(2);
-        LL3.push(4);
-        LL3.push(0);
-        LL3.push(1);
-        LinkedList<Integer> LL4 = BigNumArithmetic.addition(LL, LL2);
-        assertEquals(LL3.toString(), LL4.toString());
+
+
+      LL.push(1);
+      LL2.push(2);
+      LinkedList<Integer> LL3 = BigNumArithmetic.addition(LL, LL2);
+      assertEquals(LL3.toString(), "3");    
+      LL = new LinkedList<Integer>();
+      LL2 = new LinkedList<Integer>();
+      LL3 =new LinkedList<Integer>();
+      LL.push(1);
+      LL.push(2);
+      LL.push(7);
+      LL2.push(1);
+      LL2.push(2);
+      LL2.push(3);
+      
+      LL3.push(2);
+      LL3.push(4);
+      LL3.push(0);
+      LL3.push(1);
+      LinkedList<Integer> LL4 = BigNumArithmetic.addition(LL, LL2);
+      assertEquals(LL3.toString(), LL4.toString());
         
-        LL2.push(4);
-        LL2.push(5);
-        LL4 = BigNumArithmetic.addition(LL, LL2);
-        LL3 = new LinkedList<Integer>();
-        LL3.push(2);
-        LL3.push(4);
-        LL3.push(0);
-        LL3.push(5);
-        LL3.push(5);
-
-        System.out.println(LL.toString());
-        System.out.println(LL2.toString());
-        System.out.println(LL3.toString());
-        System.out.println(LL4.toString());
-        assertEquals(LL3.toString(), LL4.toString());    
-
+      LL2.push(4);
+      LL2.push(5);
+      LL4 = BigNumArithmetic.addition(LL, LL2);
+      LL3 = new LinkedList<Integer>();
+      LL3.push(2);
+      LL3.push(4);
+      LL3.push(0);
+      LL3.push(5);
+      LL3.push(5);
+      assertEquals(LL3.toString(), LL4.toString());        
     }
 
   
