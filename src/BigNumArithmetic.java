54dae51616ae383e3a31baaae755b0a92cd26fb7
leohan95 <53951375+leohan95@users.noreply.github.com>
2021-07-18 09:54:09 +0900

    Final changes on my side

--- src/BigNumArithmetic.java
+++ src/BigNumArithmetic.java
@@ -2,12 +2,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-/**
- * 
- * @author mhasanov, leohan95
- *
- */
-
 // On my honor:
 // - I have not used source code obtained from another student,
 // or any other unauthorized source, either modified or
@@ -27,29 +21,49 @@ import java.util.Scanner;
 // during the discussion. I have violated neither the spirit nor
 // letter of this restriction.
 
-public class BigNumArithmetic {
+/**
+ * Main class of BigNumArithmetic that runs to open the text file, read each
+ * line using the RPN by parts, handles mathematics operations and prints the
+ * results.
+ *
+ * @author mhasanov
+ * @author leohan95
+ * @version 2021.07.18
+ * @throws FileNotFoundException
+ */
+public class BigNumArithmetic
+{
 
     private static LinkedList<LinkedList<Integer>> myStack;
 
-    /*
-     * Main method ran by console comand, uses helper method
+    // ----------------------------------------------------------
+    /**
+     * Main method ran by console command, uses helper method
+     *
+     * @param args
+     *            Array of args to be passed by console
+     * @throws FileNotFoundException
      */
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args)
+        throws FileNotFoundException
+    {
         mainMethod(args);
     }
 
 
     /**
-     * Helper method for main
-     * Checks number of arguments
-     * And passes filename down to next method
-     * 
+     * Helper method for main Checks number of arguments And passes filename
+     * down to next method
+     *
      * @param args
      *            Array of args to be passed by console
      * @throws FileNotFoundException
      */
-    public static void mainMethod(String[] args) throws FileNotFoundException {
-        if (args.length != 1) {
+    public static void mainMethod(String[] args)
+        throws FileNotFoundException
+    {
+        if (args.length != 1)
+        {
             throw new IllegalArgumentException(
                 "Exactly one argument needed: file-input");
         }
@@ -59,9 +73,9 @@ public class BigNumArithmetic {
 
 
     /**
-     * Handles reading file, and writing output.
-     * Calls helper for help with calculating expression
-     * 
+     * Handles reading file, and writing output. Calls helper for help with
+     * calculating expression
+     *
      * @param filepath
      *            Name of input sfile
      * @throws FileNotFoundException
@@ -95,45 +109,49 @@ public class BigNumArithmetic {
 
     /**
      * Reads the expression and passes each part to other functions
-     * 
+     *
      * @param line
      * @return empty string if wrong expression, else string containing answer
      */
-    private static String expressionCalculator(String line) {
+    private static String expressionCalculator(String line)
+    {
         myStack = new LinkedList<LinkedList<Integer>>();
         // Break line in parts
         String[] words = line.split("\\s+");
 
         // For each part of expression
-        for (int i = 0; i < words.length; i++) {
-            
-            
+        for (int i = 0; i < words.length; i++)
+        {
 
             String word = words[i];
-            
-            
+
             // If a number
-            if (word.matches("[0-9]+")) {
+            if (word.matches("[0-9]+"))
+            {
                 char[] splitZero = stripZero(word.toCharArray());
 
                 int strLen = splitZero.length;
-                LinkedList<Integer> myLL = new LinkedList<Integer>();
-                for (int j = strLen - 1; j >= 0; j--) {
-                    myLL.push(Integer.parseInt(String.valueOf(splitZero[j])));
+                LinkedList<Integer> mylinkL = new LinkedList<Integer>();
+                for (int j = strLen - 1; j >= 0; j--)
+                {
+                    mylinkL
+                        .push(Integer.parseInt(String.valueOf(splitZero[j])));
 
                 }
-                myStack.push(myLL);
+                myStack.push(mylinkL);
 
             }
 
-            else if (word.equals("+") || word.equals("*") || word.equals("^")) {
-                
+            else if (word.equals("+") || word.equals("*") || word.equals("^"))
+            {
+
                 // If operator run function, but only if stack has more than 1
                 // otherwise we skip to next line w/continue;
-                if (myStack.getSize() < 2) {
+                if (myStack.getSize() < 2)
+                {
                     return "";
                 }
-                
+
                 operator(word);
 
             }
@@ -145,7 +163,8 @@ public class BigNumArithmetic {
         String s = answer.toString();
 
         // Check if stack is empty and answer exists
-        if (myStack.getSize() == 0 && answer != null) {
+        if (myStack.getSize() == 0 && answer != null)
+        {
             return s;
         }
         return "";
@@ -154,20 +173,24 @@ public class BigNumArithmetic {
 
     /**
      * Strips extra zeros from number, unless number is all zeros
-     * 
+     *
      * @param original
      *            char array
      * @return char array without zeros
      */
-    private static char[] stripZero(char[] original) {
+    private static char[] stripZero(char[] original)
+    {
         boolean z = true;
-        for (char ch : original) {
-            if (ch != '0') {
+        for (char ch : original)
+        {
+            if (ch != '0')
+            {
                 z = false;
                 break;
             }
         }
-        if (z) {
+        if (z)
+        {
             char[] r = new char[1];
             r[0] = '0';
             return r;
@@ -175,7 +198,8 @@ public class BigNumArithmetic {
 
         int zeros = 0;
 
-        while (zeros < original.length && original[zeros] == '0') {
+        while (zeros < original.length && original[zeros] == '0')
+        {
             zeros++;
         }
         char[] r = new char[original.length - zeros];
@@ -186,206 +210,269 @@ public class BigNumArithmetic {
     }
 
 
-    /*
-     * Determines which operation to run
+    /**
+     * Determines which operation to run by reading the keywords.
+     *
+     * @param word
+     *            recognized characters from the line of strings
      */
-    private static void operator(String word) {
-        
-        if (word.equals("+")) {
+    private static void operator(String word)
+    {
+
+        if (word.equals("+"))
+        {
             myStack.push(addition(myStack.pop(), myStack.pop()));
         }
-        else if (word.equals("*")) {
+        else if (word.equals("*"))
+        {
 
-            
             myStack.push(multiplication(myStack.pop(), myStack.pop()));
         }
-        else if (word.equals("^")) {
+        else if (word.equals("^"))
+        {
             myStack.push(exponentiation(myStack.pop(), myStack.pop()));
         }
     }
 
 
-    public static LinkedList<Integer> addition(
-        LinkedList<Integer> LL1,
-        LinkedList<Integer> LL2) {
+    // ----------------------------------------------------------
+    /**
+     * A method that processes addition operation. Takes each digit from the
+     * LinkedList and sums, adding the carry to the next digits.
+     *
+     * @param linkL1
+     *            First operand to be added.
+     * @param linkL2
+     *            Second operand to be added.
+     * @return linkL3 Returns the sum of both operands.
+     */
+    public static
+        LinkedList<Integer>
+        addition(LinkedList<Integer> linkL1, LinkedList<Integer> linkL2)
+    {
 
-        int upperBound = Math.max(LL1.getSize(), LL2.getSize());
+        int upperBound = Math.max(linkL1.getSize(), linkL2.getSize());
 
-        LinkedList<Integer> LL3 = new LinkedList<Integer>();
+        LinkedList<Integer> linkL3 = new LinkedList<Integer>();
 
         int result = 0;
         int cary = 0;
-        int a, b;
-        for (int i = 0; i < upperBound; i++) {
+        int a;
+        int b;
+        for (int i = 0; i < upperBound; i++)
+        {
 
-            if (upperBound < i) {
+            if (upperBound < i)
+            {
                 break;
             }
-            if (LL1.getSize() <= i) {
+            if (linkL1.getSize() <= i)
+            {
                 a = 0;
             }
-            else {
-                a = LL1.get(i).getData();
+            else
+            {
+                a = linkL1.get(i).getData();
             }
-            if (LL2.getSize() <= i) {
+            if (linkL2.getSize() <= i)
+            {
                 b = 0;
             }
-            else {
-                b = LL2.get(i).getData();
+            else
+            {
+                b = linkL2.get(i).getData();
             }
 
             result = (a + b + cary) % 10;
             cary = (a + b + cary) / 10;
 
-            LL3.push(result);
+            linkL3.push(result);
 
         }
-        if (cary == 1) {
-            LL3.push(cary);
+        if (cary == 1)
+        {
+            linkL3.push(cary);
         }
-        return LL3;
+
+        return linkL3;
     }
 
 
     /**
      * Processes the exponentiation using the exponentiation by squaring
-     * algorithm.
-     * x^n will be calculated differently depending on whether n is even or odd.
+     * algorithm. x^n will be calculated differently depending on whether n is
+     * even or odd.
      *
-     * @param LL2
-     *            the x variable
-     *            LL1 the n variable
-     * @return LL3 returning LinkedList output variable to x^n
+     * @param linkL2 the x variable, base variable
+     * @param linkL1 the n variable, exponent variable
+     * @return linkL3 returning LinkedList output variable to x^n
      */
-    public static LinkedList<Integer> exponentiation(
-        LinkedList<Integer> LL2,
-        LinkedList<Integer> LL1) {
-        LinkedList<Integer> LL3 = new LinkedList<Integer>();
-        LinkedList<Integer> LL4 = new LinkedList<Integer>();
+    public static
+        LinkedList<Integer>
+        exponentiation(LinkedList<Integer> linkL2, LinkedList<Integer> linkL1)
+    {
+        LinkedList<Integer> linkL3 = new LinkedList<Integer>();
+        LinkedList<Integer> linkL4 = new LinkedList<Integer>();
         LinkedList<Integer> temp = new LinkedList<Integer>();
         boolean num = false; // TRUE for odd, FALSE for even
 
-        int bSize = LL2.getSize();
+        int bSize = linkL2.getSize();
         int n = 0;
 
-        if (bSize == 1 && LL2.get(bSize - 1).getData() == 1) { // if n is 1,
-                                                               // return x
-            return LL1;
+        if (bSize == 1 && linkL2.get(bSize - 1).getData() == 1)
+        { // if n is 1,
+          // return x
+            return linkL1;
         }
 
-        if (bSize == 1 && LL2.get(bSize - 1).getData() == 0) { // if n is 0,
-                                                               // return 1
-            LL3.push(1);
-            return LL3;
+        if (bSize == 1 && linkL2.get(bSize - 1).getData() == 0)
+        { // if n is 0,
+          // return 1
+            linkL3.push(1);
+
+            return linkL3;
         }
 
-        if (LL2.get(bSize - 1).getData() % 2 == 1) { // if n is odd
+        if (linkL2.get(bSize - 1).getData() % 2 == 1)
+        { // if n is odd
             num = true;
         }
-        if (num) { // if n is odd
+        if (num)
+        { // if n is odd
 
-            for (int i = 0; i < bSize; i++) { // LL3 = n-1
-                if (i == 0) {
-                    LL3.push(LL2.get(i).getData() - 1);
+            for (int i = 0; i < bSize; i++)
+            { // linkL3 = n-1
+                if (i == 0)
+                {
+                    linkL3.push(linkL2.get(i).getData() - 1);
                 }
-                else {
-                    LL3.push(LL2.get(i).getData());
+                else
+                {
+                    linkL3.push(linkL2.get(i).getData());
                 }
 
             }
-            for (int j = 0; j < bSize; j++) { // (n-1)/2
-                LL4.push((int)Math.floor(LL3.get(j).getData() / 2));
+            for (int j = 0; j < bSize; j++)
+            { // (n-1)/2
+                linkL4.push((int)Math.floor(linkL3.get(j).getData() / 2));
             }
 
-            for (int k = 0; k < LL4.getSize(); k++) {
-                n += LL4.get(k).getData() * Math.pow(10, k);
-
-            }
-
-            LL3 = new LinkedList<Integer>(); // Reset LL3 to reuse for x^2
-            LL3 = BigNumArithmetic.multiplication(LL1, LL1); // LL3 = x^2
-            temp = LL3;
-
-            for (int x = 0; x < n - 1; x++) {
-                LL3 = BigNumArithmetic.multiplication(LL3, temp);
+            for (int k = 0; k < linkL4.getSize(); k++)
+            {
+                n += linkL4.get(k).getData() * Math.pow(10, k);
 
             }
 
-            LL3 = BigNumArithmetic.multiplication(LL3, LL1);// x(x^2)
+            linkL3 = new LinkedList<Integer>(); // Reset linkL3 to reuse for x^2
+            linkL3 = BigNumArithmetic.multiplication(
+                linkL1, linkL1); // linkL3 = x*x = x^2
+            temp = linkL3;
+
+            for (int x = 0; x < n - 1; x++)
+            {
+                linkL3 = BigNumArithmetic.multiplication(linkL3, temp);
+
+            }
+
+            linkL3 = BigNumArithmetic.multiplication(
+                linkL3, linkL1); // x(x^2)
         }
-        else {
+        else
+        {
 
-            for (int j = 0; j < bSize; j++) { // (n-1)/2
-                LL4.push((int)Math.floor(LL2.get(j).getData() / 2));
+            for (int j = 0; j < bSize; j++)
+            { // (n-1)/2
+                linkL4.push((int)Math.floor(linkL2.get(j).getData() / 2));
             }
 
-            for (int k = 0; k < LL4.getSize(); k++) {
-                n += LL4.get(k).getData() * Math.pow(10, k);
+            for (int k = 0; k < linkL4.getSize(); k++)
+            {
+                n += linkL4.get(k).getData() * Math.pow(10, k);
             }
 
-            LL3 = new LinkedList<Integer>(); // Reset LL3 to reuse for x^2
-            LL3 = BigNumArithmetic.multiplication(LL1, LL1); // LL3 = x^2
-            temp = LL3;
+            linkL3 = new LinkedList<Integer>(); // Reset linkL3 to reuse for x^2
+            linkL3 = BigNumArithmetic.multiplication(linkL1, linkL1); // linkL3
+                                                                      // = x^2
+            temp = linkL3;
 
-            for (int x = 0; x < n - 1; x++) {
-                LL3 = BigNumArithmetic.multiplication(LL3, temp);
+            for (int x = 0; x < n - 1; x++)
+            {
+                linkL3 = BigNumArithmetic.multiplication(linkL3, temp);
             }
         }
-        return LL3;
+
+        return linkL3;
     }
 
 
-    public static LinkedList<Integer> multiplication(
-        LinkedList<Integer> LL1,
-        LinkedList<Integer> LL2) {
+    // ----------------------------------------------------------
+    /**
+     * A method that processes multiplication. Each digit of a operand will be
+     * multiplied by each of the other operand, and then will be added together.
+     *
+     * @param linkL1
+     *            A operand to be multiplied
+     * @param linkL2
+     *            A operand to be multiplied
+     * @return linkL3 A linked list that represent the mutiplication of linkL1
+     *         and linkL2
+     */
+    public static
+        LinkedList<Integer>
+        multiplication(LinkedList<Integer> linkL1, LinkedList<Integer> linkL2)
+    {
 
-        
-        
-        
-        LinkedList<Integer> LL3 = new LinkedList<Integer>();
-        LinkedList<Integer> LL4;
-        LL3.push(0);
+        LinkedList<Integer> linkL3 = new LinkedList<Integer>();
+        LinkedList<Integer> linkL4;
+        linkL3.push(0);
 
-        int aSize = LL1.getSize();
-        int bSize = LL2.getSize();
-        int a, b;
+        int aSize = linkL1.getSize();
+        int bSize = linkL2.getSize();
+        int a;
+        int b;
         int carry = 0;
         int pushed;
 
-        if ((aSize == 1 && LL2.get(0).getData() == 0) || (bSize == 1 && LL2.get(
-            0).getData() == 0)) {
-
-            return LL3;
-
+        if ((aSize == 1 && linkL1.get(0).getData() == 0)
+            || (bSize == 1 && linkL2.get(0).getData() == 0))
+        {
+            return linkL3;
         }
 
-        for (int i = 0; i <= aSize - 1; i++) {
-            LL4 = new LinkedList<Integer>();
-            for (int z = 0; z < i; z++) {
-                if (i > 0) {
-                    LL4.push(0);
+        for (int i = 0; i <= aSize - 1; i++)
+        {
+            linkL4 = new LinkedList<Integer>();
+            for (int z = 0; z < i; z++)
+            {
+                if (i > 0)
+                {
+                    linkL4.push(0);
                 }
             }
 
-            for (int j = 0; j <= bSize - 1; j++) {
-                b = LL1.get(i).getData();
-                a = LL2.get(j).getData();
+            for (int j = 0; j <= bSize - 1; j++)
+            {
+                b = linkL1.get(i).getData();
+                a = linkL2.get(j).getData();
 
                 pushed = (a * b + carry) % 10;
                 carry = (a * b + carry) / 10;
 
-                LL4.push(pushed);
+                linkL4.push(pushed);
 
-                if (j == bSize - 1) {
-                    if (carry != 0) {
-                        LL4.push(carry);
+                if (j == bSize - 1)
+                {
+                    if (carry != 0)
+                    {
+                        linkL4.push(carry);
                     }
                     carry = 0;
                 }
 
             }
-            LL3 = BigNumArithmetic.addition(LL3, LL4);
+            linkL3 = BigNumArithmetic.addition(linkL3, linkL4);
         }
-        return LL3;
+
+        return linkL3;
     }
 }
